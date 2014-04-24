61f169c6fb598bc84de644db47177f818156eb4f
David Stinson <dave@passingthought.net>
2014-04-24 01:18:59 +0100

    Update join to yaml and add 'find a voice'

--- generate.py
+++ generate.py
@@ -11,6 +11,7 @@
 		the target into which the site is created
 
 """
+from docutils.core import publish_parts
 import glob
 import jinja2
 import os
@@ -25,36 +26,51 @@ def construct_yaml_str(self, node):
 yaml.Loader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
 yaml.SafeLoader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
 
-
 EMAILS = re.compile(r'\b([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})\b', re.I)
+MAILTO = re.compile(r'mailto:([^|]+)\|([^|]+)\|')
+LINK = re.compile(r'link:([^|]+)\|([^|]+)\|')
+BOLD = re.compile(r'bold:([^|]+)\|')
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 @jinja2.evalcontextfilter
 def paraText(eval_ctx, value):
 	value = jinja2.escape(value)
+	value = value.replace("\n", "\n\n")
+	value = value.replace(":\n", jinja2.Markup(":<br/>"))
 	paras = value.split("\n")
 	result = "</p>\n<p>".join(paras)
 
+#	rst = publish_parts(source=unicode(value), writer_name='html')['body']
+#	result = jinja2.Markup(rst)
+
 	# Get correct coding before making replacements for html entities
 	try:
-		result = result.replace(u"\u00A3", "&pound;")
-		result = result.replace(u'\u2013', "-")
-		result = result.replace(u'\u2019', "'")
-		result = result.replace(u'\u0159', "&#x159;")
-		result = result.replace(u'\xa0', " ")
-		result = result.replace(u"\xe1", "&aacute;")
-		result = result.replace( u'\xe9', "&eacute;")
-		result = result.replace(u'\u2018', "&#x2018;") # ‘
-		result = result.replace(u'\u2018', "&#x2019;") # ‘
-		result = result.replace(u'\u201c', "&#x201c;") # “
-		result = result.replace(u'\u201d', "&#x201d;") # ”
-		result = result.replace(u"\xfc", "&uuml;") # ü
-		result = result.replace(u'\xec', "i") # ì
-		result = EMAILS.sub(r'<a href="mailto:\1">\1</a>', result)
+		result = result.replace(u"\u00A3", jinja2.Markup("&pound;"))
+		result = result.replace(u'\u2013', jinja2.Markup("-"))
+		result = result.replace(u'\u2019', jinja2.Markup("'"))
+		result = result.replace(u'\u0159', jinja2.Markup("&#x159;"))
+		result = result.replace(u'\xa0',   jinja2.Markup(" "))
+		result = result.replace(u"\xe1",   jinja2.Markup("&aacute;"))
+		result = result.replace( u'\xe9',  jinja2.Markup("&eacute;"))
+		result = result.replace(u'\u2018', jinja2.Markup("&#x2018;")) # ‘
+		result = result.replace(u'\u2018', jinja2.Markup("&#x2019;")) # ‘
+		result = result.replace(u'\u201c', jinja2.Markup("&#x201c;")) # “
+		result = result.replace(u'\u201d', jinja2.Markup("&#x201d;")) # ”
+		result = result.replace(u"\xfc",   jinja2.Markup("&uuml;")) # ü
+		result = result.replace(u'\xec',   jinja2.Markup("i")) # ì
+
+		if MAILTO.search(result):
+			result = MAILTO.sub(r'<a href="mailto:\1">\2</a>', result)
+		else:
+			result = EMAILS.sub(r'<a href="mailto:\1">\1</a>', result)
+
+		result = LINK.sub(r'<a href="\1">\2</a>', result)
+		result = BOLD.sub(r'<b>\1</b>', result)
 		result_enc = result.decode("utf-8")
 	except Exception, exc:
 		print str(exc)
 		import pdb; pdb.set_trace()
+		raise
 
 	if eval_ctx.autoescape:
 		result_enc = jinja2.Markup(result_enc)
